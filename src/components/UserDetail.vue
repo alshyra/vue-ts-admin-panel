59de1ea9f88c0cfef1fbab9ba6f58f8356a52edd
antoine.savajols <antoine.savajols@elosi.com>
2018-06-25 18:08:56 +0200

    refonte ergo user detail

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,35 +1,19 @@
 <template>
-    <div>
-        <transition name="fade" mode="out-in">
-            <div class="ui centered inline loader active" v-if="isLoading"></div>
-        </transition>
-        <transition name="fade" mode="out-in">
-            <form class="ui form" v-if="!isLoading" @submit.prevent="saveUser">
-                <div>
-                    <div class="field ui fluid input">
-                        <input placeholder="Email" type="text" name="email" v-model="user.email">
-                    </div>
-                    <div class="ui field fluid action input">
-                        <select class="ui dropdown" v-model="user.role" name="role">
-                            <option value="ADMIN">ADMIN</option>
-                            <option value="USER">USER</option>
-                        </select>
-                        <button class="ui primary right attached button" type="submit">Ajouter</button>
-                    </div>
-                </div>
-            </form>
-        </transition>
-    </div>
+    <form class="ui form" @submit.prevent="saveUser">
+        <div>
+            <div class="field ui fluid input">
+                <input placeholder="Email" type="text" name="email" v-model="user.email">
+            </div>
+            <div class="ui field fluid action input">
+                <select class="ui dropdown" v-model="user.role" name="role">
+                    <option value="ADMIN">ADMIN</option>
+                    <option value="USER">USER</option>
+                </select>
+                <button class="ui primary right attached button" :class="{loading: isLoading}" type="submit">Ajouter</button>
+            </div>
+        </div>
+    </form>
 </template>
-<style lang="scss">
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-</style>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
